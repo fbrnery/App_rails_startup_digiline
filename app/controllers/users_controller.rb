72915102d4726c962aae22e4021e1b1707e2060c
Fabricio Miranda <fbrnery@gmail.com>
2022-11-28 15:55:13 -0300

    Gerei e configurei os controllers Sessions e Admin para autenticação de usuários

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to user_url,
+        format.html { redirect_to users_url,
         notice: "User #{@user.email} was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+
+   get 'admin' => 'admin#index'
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
   resources :users
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root 'home#index'
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
