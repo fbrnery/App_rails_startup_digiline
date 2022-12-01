1716a0291d7a97ec482bb43fc3f003f84e8b572e
Fabricio Miranda <fbrnery@gmail.com>
2022-12-01 10:20:21 -0300

    Correções no app

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   get 'sessions/destroy'
   root 'home#index'
   get 'home/index'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
